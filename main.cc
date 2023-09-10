1116ac20deb3f5d9a5219e2f32eefdc2d894500f
Li Jing <3202146864@qq.com>
2023-09-10 22:50:29 +0800

    update format

--- main.cc
+++ main.cc
@@ -1,30 +1,17 @@
 #include <drogon/drogon.h>
 
-#include <jdbc/mysql_driver.h>
-#include <jdbc/mysql_connection.h>
-
 #include "user_controller.h"
 using namespace drogon;
 
-int main()
-{
-  drogon::app().addListener("0.0.0.0", 8080);
-  std::cout << "ready" << std::endl;
-  drogon::app().registerHandler(
-      "/api/signup",
-      [](const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
-      {
+int main() {
+    drogon::app().addListener("0.0.0.0", 8080);
+    std::cout << "ready" << std::endl;
+    drogon::app().registerHandler("/api/signup", [](const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
         registerUser(req, std::move(callback));
-      });
-      drogon::app().registerHandler(
-      "/api/login",
-      [](const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
-      {
+    });
+    drogon::app().registerHandler("/api/login", [](const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
         loginUser(req, std::move(callback));
-      });
-  drogon::app().run();
-
-
-
-  return 0;
+    });
+    drogon::app().run();
+    return 0;
 }
--- user_controller.cc
+++ user_controller.cc
@@ -1,60 +1,50 @@
-#include <json/json.h>
 #include <drogon/drogon.h>
+#include <json/json.h>
+
 #include "mysql.h"
 using namespace drogon;
 
-void registerUser(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
-{
+void registerUser(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
     auto body = req->getBody();
     Json::Value root;
     Json::Reader reader;
     std::string bodyStr(body);
-    if (!reader.parse(bodyStr, root))
-    {
+    if (!reader.parse(bodyStr, root)) {
         callback(HttpResponse::newHttpResponse());
         return;
     }
     Json::FastWriter writer;
     auto output = writer.write(root);
-
     auto res = HttpResponse::newHttpResponse();
     res->addHeader("Access-Control-Allow-Origin", "*");
-    std::cout <<"username :"<< root["username"].asString() << std::endl;
-    std::cout <<"passwd :"<< root["password"].asString() << std::endl;
+    std::cout << "username :" << root["username"].asString() << std::endl;
+    std::cout << "passwd :" << root["password"].asString() << std::endl;
     res->setBody(output);
-    if(sql_check(root["username"].asString())) 
+    if (sql_check(root["username"].asString()))
         sql_add(root["username"].asString(), root["password"].asString());
-    else std::cout<<"sblgy"<<std::endl; 
-
+    else
+        std::cout << "User already exist" << std::endl;
     callback(res);
 }
-void loginUser(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
-{
+void loginUser(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
     auto body = req->getBody();
     Json::Value root;
     Json::Reader reader;
     std::string bodyStr(body);
-    if (!reader.parse(bodyStr, root))
-    {
+    if (!reader.parse(bodyStr, root)) {
         callback(HttpResponse::newHttpResponse());
         return;
     }
     Json::FastWriter writer;
     auto output = writer.write(root);
-
     auto res = HttpResponse::newHttpResponse();
     res->addHeader("Access-Control-Allow-Origin", "*");
-    std::cout <<"username :"<< root["username"].asString() << std::endl;
-    std::cout <<"passwd :"<< root["password"].asString() << std::endl;
+    std::cout << "username: " << root["username"].asString() << std::endl;
+    std::cout << "password: " << root["password"].asString() << std::endl;
     res->setBody(output);
-    if(sql_check(root["username"].asString(),root["password"].asString())) 
-    {
-        std::cout<<"succ"<<std::endl;
-    }
-    else 
-    {
-        std::cout<<"no succ"<<std::endl;
-    }
-
+    if (sql_check(root["username"].asString(), root["password"].asString()))
+        std::cout << "Success" << std::endl;
+    else
+        std::cout << "Failed" << std::endl;
     callback(res);
 }
