4b9b2ab2a0734bd6ee36bb06d115167c802921c8
lglglglgy <2661896437@qq.com>
2023-09-12 15:08:05 +0800

    fix sql_check combine two function

--- mysql.h
+++ mysql.h
@@ -3,5 +3,5 @@
 #include <jdbc/mysql_connection.h>
 //#include "mysql.cc"
 void sql_add(std::string  , std::string );
-bool sql_check(std::string  , std::string );
-bool sql_check(std::string );
+bool sql_check(std::string  , std::string ="@DEFAULT@");
+//bool sql_check(std::string );
