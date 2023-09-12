cb45dcc98db0e96af502da60f0fe49202361e01e
Li Jing <3202146864@qq.com>
2023-09-12 16:13:10 +0800

    Optimize user_controller logic

--- build/run.sh
+++
@@ -1,5 +0,0 @@
- export LD_LIBRARY_PATH=/home/lgy/c++ks/mysql-connector/lib64:$LD_LIBRARY_PATH
- cmake ..
- make clean
- make -j 14
- ./flypen
--- user_controller.cc
+++ user_controller.cc
@@ -1,32 +1,9 @@
 #include <drogon/drogon.h>
 #include <json/json.h>
-
 #include "mysql.h"
 using namespace drogon;
-
-void registerUser(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
-    auto body = req->getBody();
-    Json::Value root;
-    Json::Reader reader;
-    std::string bodyStr(body);
-    if (!reader.parse(bodyStr, root)) {
-        callback(HttpResponse::newHttpResponse());
-        return;
-    }
-    Json::FastWriter writer;
-    auto output = writer.write(root);
-    auto res = HttpResponse::newHttpResponse();
-    res->addHeader("Access-Control-Allow-Origin", "*");
-    std::cout << "username :" << root["username"].asString() << std::endl;
-    std::cout << "passwd :" << root["password"].asString() << std::endl;
-    res->setBody(output);
-    if (sql_check(root["username"].asString()))
-        sql_add(root["username"].asString(), root["password"].asString());
-    else
-        std::cout << "User already exist" << std::endl;
-    callback(res);
-}
-void loginUser(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
+typedef std::string (*HandlerFunc)(const Json::Value&);
+void Handle(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback, HandlerFunc handler){
     auto body = req->getBody();
     Json::Value rec_json,res_json;
     Json::Reader reader;
@@ -36,19 +13,28 @@ void loginUser(const HttpRequestPtr &req, std::function<void(const HttpResponseP
         return;
     }
     Json::FastWriter writer;
-    std::cout << "The Collected username: " << rec_json["username"].asString() << std::endl;
-    std::cout << "The Collected password: " << rec_json["password"].asString() << std::endl;
-    if (sql_check(rec_json["username"].asString(), rec_json["password"].asString())){
-        std::cout << "Success" << std::endl;
-        //3.4,3.12
-        res_json["msg"]="Success";
-    }else{
-        std::cout << "Failed" << std::endl;
-        res_json["msg"]="Failed";
-    }
+    std::string msg = handler(rec_json);
+    res_json["msg"] = msg;
     auto output = writer.write(res_json);
     auto res = HttpResponse::newHttpResponse();
     res->addHeader("Access-Control-Allow-Origin", "*");
     res->setBody(output);
     callback(res);
+}
+std::string registerUser(const Json::Value& rec_json) {
+    std::cout<<rec_json["username"].asString()<<std::endl;
+    std::cout<<rec_json["password"].asString()<<std::endl;
+    if (sql_check(rec_json["username"].asString())){
+        sql_add(rec_json["username"].asString(), rec_json["password"].asString());
+        return "Sign up Success";
+    }else
+        return "User already exist";
+}
+std::string loginUser(const Json::Value& rec_json) {
+    std::cout<<rec_json["username"].asString()<<std::endl;
+    std::cout<<rec_json["password"].asString()<<std::endl;
+    if (sql_check(rec_json["username"].asString(), rec_json["password"].asString()))
+        return "Success";
+    else
+        return "Failed";
 }
