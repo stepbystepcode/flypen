7a74a9cb9399291b8e817508b454d19323ba0b5d
lglglglgy <2661896437@qq.com>
2023-09-14 16:28:48 +0800

    ws

--- MsgWebsock.cc
+++ MsgWebsock.cc
@@ -1,9 +1,11 @@
 #include "MsgWebsock.h"
-
+#include "jwt_controller.h"
+std::set<WebSocketConnectionPtr> MsgWebsock::connections;
 void MsgWebsock::handleNewMessage(const WebSocketConnectionPtr& wsConnPtr, std::string &&message, const WebSocketMessageType &type)
 {
     // write your application logic here
-     wsConnPtr->send(message);
+    message = jwtDecrypt(message);   
+     wsConnPtr->send("Hello, " + message + "!");
 }
 
 void MsgWebsock::handleNewConnection(const HttpRequestPtr &req, const WebSocketConnectionPtr& wsConnPtr)
@@ -13,12 +15,13 @@ void MsgWebsock::handleNewConnection(const HttpRequestPtr &req, const WebSocketC
     LOG_INFO << "New WebSocket connection from IP: " << clientIP;
 
     // 添加连接到集合
-    //connections.insert(wsConnPtr);
+    connections.insert(wsConnPtr);
 
 
     // 在此处执行任何其他初始化操作
     // 例如，向连接发送欢迎消息
     wsConnPtr->send("Welcome to the WebSocket server! The connect has been established successfully.");
+
 }
 
 void MsgWebsock::handleConnectionClosed(const WebSocketConnectionPtr& wsConnPtr)
