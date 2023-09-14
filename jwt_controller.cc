5d544a1d922e88da01a16c515b3f85296a1d87be
lglglglgy <2661896437@qq.com>
2023-09-14 15:54:13 +0800

    websocket

--- jwt_controller.cc
+++ jwt_controller.cc
@@ -11,3 +11,17 @@ std::string jwtGen(const Json::Value& rec_json)
     .sign(jwt::algorithm::hs256{"secret"});
     return std::string(token);
 }
+void jwtVerify(const std::string& token)
+{
+    try
+    {
+        auto decoded = jwt::decode(token);
+        std::cout << "Header: " << decoded.get_header() << std::endl;
+        std::cout << "Payload: " << decoded.get_payload() << std::endl;
+        std::cout << "Signature: " << decoded.get_signature() << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << "Error info: " << e.what() << std::endl;
+    }
+}
--- jwt_controller.h
+++ jwt_controller.h
@@ -3,4 +3,5 @@
 #include <json/json.h>
 #include <cstring>
 std::string jwtGen(const Json::Value& rec_json);
+void jwtVerify(const std::string& token);
 #endif
