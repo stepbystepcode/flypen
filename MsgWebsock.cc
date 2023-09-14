5f840465788894d63f152ef911d195e70fd6aef3
lglglglgy <2661896437@qq.com>
2023-09-14 15:54:13 +0800

    websocket

--- MsgWebsock.cc
+++ MsgWebsock.cc
@@ -9,6 +9,16 @@ void MsgWebsock::handleNewMessage(const WebSocketConnectionPtr& wsConnPtr, std::
 void MsgWebsock::handleNewConnection(const HttpRequestPtr &req, const WebSocketConnectionPtr& wsConnPtr)
 {
     // write your application logic here
+      const std::string &clientIP = req->peerAddr().toIp();
+    LOG_INFO << "New WebSocket connection from IP: " << clientIP;
+
+    // 添加连接到集合
+    //connections.insert(wsConnPtr);
+
+
+    // 在此处执行任何其他初始化操作
+    // 例如，向连接发送欢迎消息
+    wsConnPtr->send("Welcome to the WebSocket server! The connect has been established successfully.");
 }
 
 void MsgWebsock::handleConnectionClosed(const WebSocketConnectionPtr& wsConnPtr)
