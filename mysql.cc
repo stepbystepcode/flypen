b5c040ea2b9956bc7d8076c2d72ab903d853a9db
Jing Li <abc1870819884@gmail.com>
2023-09-15 17:08:44 +0800

    rename fixbug no_token_deined_connect_ws
    find a bug in send ws message

--- mysql.cc
+++ mysql.cc
@@ -4,8 +4,10 @@
 #include <jdbc/mysql_connection.h>
 #include "jdbc/mysql_driver.h"
 #include "json/json.h"
-void sql_addconnect(std::string connectptr){
-    try{
+void sql_addconnect(std::string connectptr)
+{
+    try
+    {
         sql::mysql::MySQL_Driver *driver;
         driver = sql::mysql::get_mysql_driver_instance();
         sql::Connection *con;
@@ -15,17 +17,20 @@ void sql_addconnect(std::string connectptr){
         tool = con->createStatement();
         std::string classmysql = "INSERT INTO users(username,password,time,connection) VALUES (?,?,NOW(),?)";
         sql::PreparedStatement *insertconnect = con->prepareStatement(classmysql);
-        insertconnect->setString(4,connectptr);
+        insertconnect->setString(4, connectptr);
         delete insertconnect;
         delete tool;
         delete con;
     }
-    catch (sql::SQLException &e) {
+    catch (sql::SQLException &e)
+    {
         std::cerr << "SQL Exception: " << e.what() << std::endl;
     }
 }
-void sql_addhistory(std::string sender,std::string receiver,std::string message,std::string isread){
-    try{
+void sql_addhistory(std::string sender, std::string receiver, std::string message, std::string isread)
+{
+    try
+    {
         sql::mysql::MySQL_Driver *driver;
         driver = sql::mysql::get_mysql_driver_instance();
         sql::Connection *con;
@@ -33,33 +38,36 @@ void sql_addhistory(std::string sender,std::string receiver,std::string message,
         con->setSchema("flypen");
         sql::Statement *tool;
         tool = con->createStatement();
-        std::string classmysql = "INSERT INTO chat(content,isread, receiver,sender,time) VALUES (?,?,?,?,NOW())";
+        std::string classmysql = "INSERT INTO chat(content,isread, sender,receiver,time) VALUES (?,?,?,?,NOW())";
         sql::PreparedStatement *insertdata = con->prepareStatement(classmysql);
         insertdata->setString(1, message);
-        insertdata->setString(2,isread);
-        insertdata->setString(3,sender);
-        insertdata->setString(4,receiver);
+        insertdata->setString(2, isread);
+        insertdata->setString(3, sender);
+        insertdata->setString(4, receiver);
         insertdata->executeUpdate();
         delete insertdata;
         delete tool;
         delete con;
     }
-    catch (sql::SQLException &e) {
+    catch (sql::SQLException &e)
+    {
         std::cerr << "SQL Exception: " << e.what() << std::endl;
     }
 }
-void sql_add(std::string username, std::string passwd) {
-    try {
+void sql_add(std::string username, std::string passwd)
+{
+    try
+    {
         sql::mysql::MySQL_Driver *driver;
         driver = sql::mysql::get_mysql_driver_instance();
         sql::Connection *con;
-        //con = driver->connect("tcp://localhost:3306", "root", "abc.123");
-        //con = driver->connect("tcp://192.168.31.78:3306", "root", "abc.123");
+        // con = driver->connect("tcp://localhost:3306", "root", "abc.123");
+        // con = driver->connect("tcp://192.168.31.78:3306", "root", "abc.123");
         con = driver->connect("tcp://8.130.48.157:3306", "root", "abc.123");
         con->setSchema("flypen");
         sql::Statement *tool;
         tool = con->createStatement();
-       std::string classmysql = "INSERT INTO users(username, password, createtime) VALUES (?, ?, NOW())";
+        std::string classmysql = "INSERT INTO users(username, password, createtime) VALUES (?, ?, NOW())";
 
         sql::PreparedStatement *ptool = con->prepareStatement(classmysql);
         ptool->setString(1, username);
@@ -68,10 +76,11 @@ void sql_add(std::string username, std::string passwd) {
         delete ptool;
         delete tool;
         delete con;
-    } catch (sql::SQLException &e) {
+    }
+    catch (sql::SQLException &e)
+    {
         std::cerr << "SQL Exception: " << e.what() << std::endl;
     }
-    
 }
 // bool sql_check(std::string user) {
 //     bool result = false;
@@ -109,32 +118,35 @@ void sql_add(std::string username, std::string passwd) {
 
 //     return result;
 // }
-bool sql_check(std::string user, std::string passwd ) {
+bool sql_check(std::string user, std::string passwd)
+{
     bool result = false;
-    try {
+    try
+    {
         sql::mysql::MySQL_Driver *driver;
         driver = sql::mysql::get_mysql_driver_instance();
         sql::Connection *con;
-        //con = driver->connect("tcp://127.0.0.1:3306", "root", "abc.123");
-        //con = driver->connect("tcp://192.168.31.78:3306", "root", "abc.123");
+        // con = driver->connect("tcp://127.0.0.1:3306", "root", "abc.123");
+        // con = driver->connect("tcp://192.168.31.78:3306", "root", "abc.123");
         con = driver->connect("tcp://8.130.48.157:3306", "root", "abc.123");
         con->setSchema("flypen");
         std::string sql = "SELECT * FROM users WHERE username = ? LIMIT 1";
         sql::PreparedStatement *prepStmt = con->prepareStatement(sql);
         prepStmt->setString(1, user);
         sql::ResultSet *res = prepStmt->executeQuery();
-      //    std::cout << passwd << std::endl;
+        //    std::cout << passwd << std::endl;
 
         // 获取查询结果
         // (¬A ∧B) ∨ (A ∧¬B)
-        if (((passwd != "@DEFAULT@")&&(res->next()))||((passwd == "@DEFAULT@")&&(!(res->next())))) {
+        if (((passwd != "@DEFAULT@") && (res->next())) || ((passwd == "@DEFAULT@") && (!(res->next()))))
+        {
             result = true;
-            //std::cout << "in" << std::endl;
-             //提取所有列的值
+            // std::cout << "in" << std::endl;
+            // 提取所有列的值
             std::string username = res->getString("username");
             std::string password = res->getString("password");
             int createtime = res->getInt("createtime");
-            if ( (passwd !=password)&&(passwd != "@DEFAULT@") )
+            if ((passwd != password) && (passwd != "@DEFAULT@"))
                 result = false;
             // 在这里输出或使用提取的值
             // std::cout << "SQL: Username: " << username << std::endl;
@@ -145,17 +157,20 @@ bool sql_check(std::string user, std::string passwd ) {
         delete res;
         delete prepStmt;
         delete con;
-    } catch (sql::SQLException &e) {
+    }
+    catch (sql::SQLException &e)
+    {
         std::cerr << "SQL Exception: " << e.what() << std::endl;
     }
-    std::cout << result<< std::endl;
+    std::cout << result << std::endl;
 
     return result;
 }
-std::string  sql_find_my_msg(std::string user)
-{   
-    std::cout<<"login user : "<<user<<std::endl;
-    try {
+std::string sql_find_my_msg(std::string user)
+{
+    std::cout << "login user: " << user << std::endl;
+    try
+    {
         sql::mysql::MySQL_Driver *driver;
         driver = sql::mysql::get_mysql_driver_instance();
         sql::Connection *con;
@@ -165,24 +180,24 @@ std::string  sql_find_my_msg(std::string user)
         sql::PreparedStatement *prepStmt = con->prepareStatement(sql);
         prepStmt->setString(1, user);
         sql::ResultSet *res = prepStmt->executeQuery();
-        std::string result ="{ ";
+        std::string result = "{ ";
 
         // 获取查询结果
-        while ((res->next())) {
-            result += "\"" + res->getString("sender") + "\":\"" + res->getString("content") + "\",";          
+        while ((res->next()))
+        {
+            result += "\"" + res->getString("sender") + "\":\"" + res->getString("content") + "\",";
             // 提取当前行的值
-
         }
         result += "}";
-        std::cout <<"msgs :"<< result << std::endl;
+        std::cout << "msgs :" << result << std::endl;
         delete res;
         delete prepStmt;
         delete con;
         return result;
-    } catch (sql::SQLException &e) {
-         std::cerr << "SQL Exception: " << e.what() << std::endl;
-         return "error";
     }
-
-    
+    catch (sql::SQLException &e)
+    {
+        std::cerr << "SQL Exception: " << e.what() << std::endl;
+        return "error";
+    }
 }
