5e861c4e11199be8a34b1ad328b1d994a69f4661
Li Jing <3202146864@qq.com>
2023-09-16 22:41:33 +0800

    stepbystep

--- msg_controller.cc
+++ msg_controller.cc
@@ -6,31 +6,39 @@
 #include "jwt_controller.h"
 #include "mysql.h"
 using namespace drogon;
-void chat(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
+void chat(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
+{
     auto body = req->getBody();
     Json::Value req_json, res_json;
     Json::Reader reader;
     std::string bodyStr(body);
-    if (!reader.parse(bodyStr, req_json)) {
+    if (!reader.parse(bodyStr, req_json))
+    {
         callback(HttpResponse::newHttpResponse());
         return;
     }
     Json::FastWriter writer;
     std::string authHeader = req->getHeader("Authorization");
-    if (authHeader.substr(0, 7) == "Bearer ") {
+    if (authHeader.substr(0, 7) == "Bearer ")
+    {
         std::string bearerToken = authHeader.substr(7);
         // 在此处使用Bearer Token进行身份验证
-        try {
+        try
+        {
             std::string sender = jwtDecrypt(bearerToken);
             std::string content = req_json["content"].asString();
             std::string receiver = req_json["receiver"].asString();
             std::cout << "Connect success: " << sender << std::endl;
             sql_addhistory(sender, receiver, content, "0");
-        } catch (const std::exception &e) {
+        }
+        catch (const std::exception &e)
+        {
             std::cerr << e.what() << '\n';
             std::cout << "Wrong token" << std::endl;
         }
-    } else {
+    }
+    else
+    {
         // 连接没有Authorization头部Bearer Token
         std::cout << "No Authorization" << std::endl;
     }
@@ -41,28 +49,36 @@ void chat(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)
     res->setBody(output);
     callback(res);
 }
-void check(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
+void check(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
+{
     auto body = req->getBody();
     Json::Value req_json, res_json;
     Json::Reader reader;
     std::string bodyStr(body);
-    if (!reader.parse(bodyStr, req_json)) {
+    if (!reader.parse(bodyStr, req_json))
+    {
         callback(HttpResponse::newHttpResponse());
         return;
     }
     std::string me, who_send_me;
     Json::FastWriter writer;
     std::string authHeader = req->getHeader("Authorization");
-    if (authHeader.substr(0, 7) == "Bearer ") {
+    if (authHeader.substr(0, 7) == "Bearer ")
+    {
         std::string bearerToken = authHeader.substr(7);
         // 在此处使用Bearer Token进行身份验证
-        try {
+        try
+        {
             me = jwtDecrypt(bearerToken);
-        } catch (const std::exception &e) {
+        }
+        catch (const std::exception &e)
+        {
             std::cerr << e.what() << '\n';
             std::cout << "Wrong token" << std::endl;
         }
-    } else {
+    }
+    else
+    {
         // 连接没有Authorization头部Bearer Token
         std::cout << "No Authorization" << std::endl;
     }
@@ -72,5 +88,52 @@ void check(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &
     who_send_me = req_json["person"].asString();
     auto output = writer.write(sql_find_my_msg(me, who_send_me));
     res->setBody(output);
+    callback(res);
+}
+void friend_request(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
+{
+    std::string authHeader = req->getHeader("Authorization");
+    if (authHeader.substr(0, 7) == "Bearer ")
+    {
+        std::string bearerToken = authHeader.substr(7);
+        // 在此处使用Bearer Token进行身份验证
+        try
+        {
+            std::string sender = jwtDecrypt(bearerToken);
+            std::string receiver = req->getParameter("username");
+            sql_addrequest(sender, receiver);
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+            std::cout << "Wrong token" << std::endl;
+        }
+    }
+    auto res = HttpResponse::newHttpResponse();
+    res->setBody("Success");
+    callback(res);
+}
+void request_processing(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
+{
+    std::string authHeader = req->getHeader("Authorization");
+    if (authHeader.substr(0, 7) == "Bearer ")
+    {
+        std::string bearerToken = authHeader.substr(7);
+        // 在此处使用Bearer Token进行身份验证
+        try
+        {
+            std::string receiver = jwtDecrypt(bearerToken);
+            std::string sender =req->getParameter("username");
+            std::string attitude = req->getParameter("info");
+            sql_process_request(sender,receiver, attitude);
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+            std::cout << "Wrong token" << std::endl;
+        }
+    }
+    auto res = HttpResponse::newHttpResponse();
+    res->setBody("Success");
     callback(res);
 }
--- msg_controller.h
+++ msg_controller.h
@@ -4,4 +4,7 @@
 using namespace drogon;
 void chat(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
 void check(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
+void search(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
+void request_processing(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
+void friend_request(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
 #endif
