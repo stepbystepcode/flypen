214e48e7aec938b82982fb1f4538090b9c00125d
Li Jing <3202146864@qq.com>
2023-09-19 12:20:46 +0800

    Extract jwt Validation Logic Functions

--- jwt_controller.cc
+++ jwt_controller.cc
@@ -1,9 +1,12 @@
-#include "jwt_controller.h"
 #include <iostream>
+#include <drogon/drogon.h>
 #include <jwt-cpp/jwt.h>
 #include <cstring>
 #include <chrono>
+#include "jwt_controller.h"
+
 using namespace jwt;
+
 std::string jwtGen(const Json::Value& req_json)
 {
     auto now = std::chrono::system_clock::now();
@@ -34,5 +37,21 @@ std::string jwtDecrypt(const std::string& token)
     } catch (const std::exception& e) {
         std::cout<<"Failed to decrypt JWT: " + std::string(e.what())<<std::endl;
         throw std::runtime_error("Failed to decrypt JWT");
+    }
+}
+bool jwtVerify(const std::__1::shared_ptr<drogon::HttpRequest> &req){
+    std::string authHeader = req->getHeader("Authorization");
+    if (authHeader.substr(0, 7) == "Bearer ") {
+        std::string bearerToken = authHeader.substr(7);
+        try {
+            std::string sender = jwtDecrypt(bearerToken);
+            return true;
+        } catch (const std::exception &e) {
+            std::cout << "Wrong token" << std::endl;
+            return false;
+        }
+    } else {
+        std::cout << "No Authorization" << std::endl;
+        return false;
     }
 }
--- jwt_controller.h
+++ jwt_controller.h
@@ -1,7 +1,12 @@
 #ifndef _JWT_CONTROLLER_H_
 #define _JWT_CONTROLLER_H_
+
+#include <drogon/drogon.h>
 #include <json/json.h>
 #include <cstring>
+
 std::string jwtGen(const Json::Value& req_json);
 std::string jwtDecrypt(const std::string& token);
+bool jwtVerify(const std::__1::shared_ptr<drogon::HttpRequest> &req);
+
 #endif
