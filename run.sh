522dcaa3234fdc1f66621dd65927d19d8b74b7b8
Jing Li <57084184+stepbystepcode@users.noreply.github.com>
2023-09-22 15:39:56 +0800

    Update run.sh, add ascii art

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+echo "███████╗██╗░░░░░██╗░░░██╗██████╗░███████╗███╗░░██╗"
+echo "██╔════╝██║░░░░░╚██╗░██╔╝██╔══██╗██╔════╝████╗░██║"
+echo "█████╗░░██║░░░░░░╚████╔╝░██████╔╝█████╗░░██╔██╗██║"
+echo "██╔══╝░░██║░░░░░░░╚██╔╝░░██╔═══╝░██╔══╝░░██║╚████║"
+echo "██║░░░░░███████╗░░░██║░░░██║░░░░░███████╗██║░╚███║"
+echo "╚═╝░░░░░╚══════╝░░░╚═╝░░░╚═╝░░░░░╚══════╝╚═╝░░╚══╝"
  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/mysql-connector/lib64
  export DYLD_LIBRARY_PATH=/Users/stepbystep/Documents/flypen/mysql-connector/lib64:$DYLD_LIBRARY_PATH
  cd build
